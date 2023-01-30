f0ed6e3bf6d5e3d46d2192a53bf4ff47141ff865
robinmartenssss <robin.martens2@student.howest.be>
2023-01-30 02:14:04 +0100

    horizontal klaaar buiten overflow nog zoeken nu interacties

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
+import { VitePluginFonts } from "vite-plugin-fonts";
+
 export default {
   base: "/int3/",
+  plugins: [
+    VitePluginFonts({
+      google: {
+        families: ["IBM Plex Sans"],
+      },
+    }),
+  ],
 };
